c4812362ca0d38b8d6a66832967901f80cdc2b32
0xreentrant <0x7e1e@3six9.io>
2024-02-03 04:35:04 -0700

    vault backup: 2024-02-03 04:35:04

--- Scripts/get-pool-oracle.js
+++
@@ -1,4 +0,0 @@
-pool = await ethers.getContractAt("LendingPool", "0x6e5d1a9C405DF46a8503893DBa99c3af5Dc1FB5A")
-provAddr = await pool.getAddressesProvider()
-prov = await ethers.getContractAt("LendingPoolAddressesProvider", provAddr)
-oracle = await ethers.getContractAt("AaveOracle", await prov.getPriceOracle())
